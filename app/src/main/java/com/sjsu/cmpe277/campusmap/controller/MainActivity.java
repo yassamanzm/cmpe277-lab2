45903107b0248229fe90dc45cc981414e6ccd7c8
Yassaman Zand <yassamanzm@gmail.com>
2016-10-28 05:27:33 -0700

    Finding current location of the user. I'm not sure if it's the emulator or the fragment that updated location is not returned. Still need to test it more.

--- app/src/main/java/com/sjsu/cmpe277/campusmap/controller/MainActivity.java
+++ app/src/main/java/com/sjsu/cmpe277/campusmap/controller/MainActivity.java
@@ -5,23 +5,46 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.sjsu.cmpe277.campusmap.R;
 
 public class MainActivity extends FragmentActivity {
+
+    private static final int REQUEST_ERROR = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        FragmentManager fm= getSupportFragmentManager();
-        Fragment fragment= fm.findFragmentById(R.id.fragment_map_container);
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_map_container);
 
         if (fragment == null) {
-            fragment= new MapFragment();
+            fragment = new MapFragment();
             fm.beginTransaction()
                     .add(R.id.fragment_map_container, fragment)
                     .commit();
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        checkPlayServices();
+    }
+
+    private boolean checkPlayServices() {
+        GoogleApiAvailability googleAPI = GoogleApiAvailability.getInstance();
+        int result = googleAPI.isGooglePlayServicesAvailable(this);
+        if (result != ConnectionResult.SUCCESS) {
+            if (googleAPI.isUserResolvableError(result)) {
+                googleAPI.getErrorDialog(this, result, REQUEST_ERROR).show();
+            }
+            return false;
+        }
+        return true;
     }
 }
--- app/src/main/java/com/sjsu/cmpe277/campusmap/controller/MapFragment.java
+++ app/src/main/java/com/sjsu/cmpe277/campusmap/controller/MapFragment.java
@@ -1,24 +1,58 @@
 package com.sjsu.cmpe277.campusmap.controller;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.sjsu.cmpe277.campusmap.R;
 import com.sjsu.cmpe277.campusmap.view.CampusView;
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener{
+
+    private static final String TAG = "MapFragment";
+    private static final int PERMISSIONS_REQUEST_ACCESS_LOCATION = 1;
+
+    private double mCurrentLatitude;
+    private double mCurrentLongitude;
 
     private CampusView mCampusView;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(5000); //5 seconds
+        mLocationRequest.setFastestInterval(3000); //3 seconds
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
     }
 
     @Nullable
@@ -27,12 +61,12 @@ public class MapFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
 
-        View v= inflater.inflate(R.layout.fragment_map, container, false);
-        mCampusView= (CampusView)v.findViewById(R.id.campus_view);
+        View v = inflater.inflate(R.layout.fragment_map, container, false);
+        mCampusView = (CampusView) v.findViewById(R.id.campus_view);
         mCampusView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                Intent buildingIntent= new Intent(getActivity(), BuildingActivity.class);
+                Intent buildingIntent = new Intent(getActivity(), BuildingActivity.class);
 //                streetIntent.putExtra(Key, Value);
                 startActivity(buildingIntent);
                 return false;
@@ -40,5 +74,91 @@ public class MapFragment extends Fragment {
         });
 
         return v;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mGoogleApiClient.isConnected()) {
+           getCurrentLocation();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, do the task you need to do.
+                    getCurrentLocation();
+                } else {
+                    // permission denied, disable functionality that depends on this permission.
+                    Toast.makeText(getActivity(), R.string.location_permission_denied, Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        getCurrentLocation();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        // ToDo
+        Toast.makeText(getActivity(), "onConnectionFailed", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        // ToDo
+        Toast.makeText(getActivity(), "onConnectionSuspended", Toast.LENGTH_SHORT).show();
+    }
+
+    private void getCurrentLocation() {
+
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // No explanation needed, we can request the permission.
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_LOCATION);
+            return;
+        }
+        Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (lastLocation != null) {
+            mCurrentLatitude = lastLocation.getLatitude();
+            mCurrentLongitude = lastLocation.getLongitude();
+            // show the marker on the map
+            Log.d(TAG, String.format("last known Location is: (%f, %f)",
+                    mCurrentLatitude, mCurrentLongitude));
+        }
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLongitude = location.getLongitude();
+        mCurrentLatitude = location.getLatitude();
+        Log.d(TAG, String.format("Current Location is: (%f, %f)",
+                mCurrentLatitude, mCurrentLongitude));
     }
 }
