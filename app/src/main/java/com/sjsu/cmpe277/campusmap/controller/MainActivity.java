73e51622a136bf470ebb6beade03d74de70ed340
Bharath Kumar Narahari <naraharibharathkumar@gmail.com>
2016-10-28 10:25:54 -0700

    Merge branch 'master' into bkNarahari
    # Conflicts:
    #	app/src/main/java/com/sjsu/cmpe277/campusmap/controller/MainActivity.java

--- app/src/main/java/com/sjsu/cmpe277/campusmap/controller/MainActivity.java
+++ app/src/main/java/com/sjsu/cmpe277/campusmap/controller/MainActivity.java
@@ -1,50 +1,42 @@
 package com.sjsu.cmpe277.campusmap.controller;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
 import com.sjsu.cmpe277.campusmap.R;
 
-public class MainActivity extends FragmentActivity {
-
-    private static final int REQUEST_ERROR = 0;
+public class MainActivity extends FragmentActivity implements View.OnTouchListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_map_container);
+        ImageView iv = (ImageView) findViewById (R.id.image);
+        if (iv != null) {
+            iv.setOnTouchListener (this);
+        }
+        Toast.makeText(getApplicationContext(), "Touch the screen to discover where the regions are.", Toast.LENGTH_LONG);
+/*
+        FragmentManager fm= getSupportFragmentManager();
+        Fragment fragment= fm.findFragmentById(R.id.fragment_map_container);
 
         if (fragment == null) {
-            fragment = new MapFragment();
+            fragment= new MapFragment();
             fm.beginTransaction()
                     .add(R.id.fragment_map_container, fragment)
                     .commit();
         }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        checkPlayServices();
-    }
-
-    private boolean checkPlayServices() {
-        GoogleApiAvailability googleAPI = GoogleApiAvailability.getInstance();
-        int result = googleAPI.isGooglePlayServicesAvailable(this);
-        if (result != ConnectionResult.SUCCESS) {
-            if (googleAPI.isUserResolvableError(result)) {
-                googleAPI.getErrorDialog(this, result, REQUEST_ERROR).show();
-            }
-            return false;
-        }
-        return true;
+        */
     }
 }
