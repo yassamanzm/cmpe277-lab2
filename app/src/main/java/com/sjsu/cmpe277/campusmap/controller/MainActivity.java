d0a682cc565bf4cf54b276fd1778197582cdfa1d
Bharath Kumar Narahari <naraharibharathkumar@gmail.com>
2016-10-29 21:55:27 -0700

    Added User location on click with display pending

--- app/src/main/java/com/sjsu/cmpe277/campusmap/controller/MainActivity.java
+++ app/src/main/java/com/sjsu/cmpe277/campusmap/controller/MainActivity.java
@@ -60,6 +60,25 @@ public class MainActivity extends FragmentActivity implements View.OnTouchListen
 
     private ImageView mCampusImage;
 
+    private int rect_1_x = 94;
+    private int rect_1_y = 734;
+    private int rect_2_x = 1295;
+    private int rect_2_y = 723;
+    private int rect_3_x = 94;
+    private int rect_3_y = 1736;
+    private int rect_4_x = 1295;
+    private int rect_4_y = 1732;
+
+    private double rect_a_x = 37.335823;
+    private double rect_a_y = -121.885946;
+    private double rect_b_x = 37.338794;
+    private double rect_b_y = -121.879736;
+    private double rect_c_x = 37.331637;
+    private double rect_c_y = -121.882829;
+    private double rect_d_x = 37.334589;
+    private double rect_d_y = -121.876582;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -96,8 +115,9 @@ public class MainActivity extends FragmentActivity implements View.OnTouchListen
                         "("+ mCurrentLocation.getLatitude() + ", " + mCurrentLocation.getLongitude() + ")", Toast.LENGTH_LONG).show();
 
                 // TODO:  calculate x and y here
-
-                drawUserLocation(500, 500);
+                int x = (int)calculateX(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+                int y = (int)calculateY(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+                drawUserLocation(x,y);
 
             }
         });
@@ -107,6 +127,57 @@ public class MainActivity extends FragmentActivity implements View.OnTouchListen
         buildLocationSettingsRequest();
         checkLocationSettings();
 
+    }
+
+    public double calculateX(double x, double y){
+        double theta = 0.0;
+        double lat = 0.0;
+        double lon = 0.0;
+        if(x > rect_a_x){
+            double x1 = Math.abs(x) - Math.abs(rect_a_x);
+            x1 = Math.pow(x1, 2);
+            double y1 = Math.abs(y)- Math.abs(rect_a_y);
+            y1 = Math.pow(y1, 2);
+            double p = Math.sqrt(Math.pow((Math.abs(x) - Math.abs(rect_a_x)), 2) - Math.pow((Math.abs(y) - Math.abs(rect_a_y)), 2));
+            theta = Math.sin((x-rect_a_x)/p);
+            theta = 20 - theta;
+            lat = rect_a_x - (p*Math.sin(theta));
+            lon = rect_a_y + (p*Math.cos(theta));
+        }
+        else{
+            double x1 = Math.abs(rect_a_x) - Math.abs(x);
+            x1 = Math.pow(x1, 2);
+            double y1 = Math.abs(rect_a_y) - Math.abs(y);
+            y1 = Math.pow(y1, 2);
+            double p = Math.sqrt(Math.pow((Math.abs(rect_a_x) - Math.abs(x)), 2) + Math.pow((Math.abs(rect_a_y) - Math.abs(y)), 2));
+            theta = Math.sin((rect_a_x-x)/p);
+            theta = 20 + theta;
+            lat = rect_a_x - (p*Math.sin(theta));
+            lon = rect_a_y + (p*Math.cos(theta));
+        }
+        double px =(((rect_2_x - rect_1_x)*(lon - rect_a_y))/(rect_c_y - rect_a_y)) + (double)rect_1_x ;
+        return px;
+    }
+
+    public double calculateY(double x, double y){
+        double p = Math.sqrt(Math.pow((Math.abs(x) - Math.abs(rect_a_x)), 2) + Math.pow((Math.abs(y) - Math.abs(rect_a_y)), 2));
+        double theta = 0.0;
+        double lat = 0.0;
+        double lon = 0.0;
+        if(x > rect_a_x){
+            theta = Math.sin((x-rect_a_x)/p);
+            theta = 20 - theta;
+            lat = rect_a_x - (p*Math.sin(theta));
+            lon = rect_a_y + (p*Math.cos(theta));
+        }
+        else{
+            theta = Math.sin((rect_a_x-x)/p);
+            theta = 20 + theta;
+            lat = rect_a_x - (p*Math.sin(theta));
+            lon = rect_a_y + (p*Math.cos(theta));
+        }
+        double py =(((rect_3_y - rect_1_y)*(lat - rect_a_x))/(rect_c_x - rect_a_x)) + (double)rect_1_y ;
+        return py;
     }
 
     @Override
